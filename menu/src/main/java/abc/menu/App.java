c6afbb7ad13943cce3f840426992ca05b57ad59e
Tianshu Bao <tbao@tbao-mbp.local.dhcp.thefacebook.com>
2018-06-17 16:05:25 -0700

    first commit

---
+++ menu/src/main/java/abc/menu/App.java
@@ -0,0 +1,13 @@
+package abc.menu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
